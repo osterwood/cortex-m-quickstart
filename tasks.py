7ca7e8afa8666ac4167c66ebb99047e3c7a4347c
Chris Osterwood <osterwood@gmail.com>
2018-11-01 21:43:32 -0700

    Now switch build target when MCU changes

--- tasks.py
+++ tasks.py
@@ -24,3 +24,32 @@ def target(c, mcu):
 
         with open('memory.x', 'w') as output_file:
             output_file.write(output)
+
+    config = []
+    mode   = "prebuild"
+    with open('.cargo/config') as input_file:
+        for line in input_file.readlines():
+
+            # Replace the first line with the proper target
+            # Doesn't really need to be done as we'll be executing
+            # QEMU directly via invoke tasks.
+            if line.startswith("[target."):
+                line = "[target.{}]".format(data[mcu]['target'])
+
+            if mode == "inbuild":
+                if "\"{}\"".format(data[mcu]['target']) in line:
+                    # Make sure the line is active
+                    line = line.replace("# target", "target").replace("#target", "target")
+                else:
+                    # Make sure the line is commented out
+                    if line[0] != "#":
+                        line = "# " + line.strip()
+
+            if line.startswith("[build]"):
+                mode = "inbuild"
+
+            config.append(line.strip())
+
+    with open('.cargo/config', 'w') as output_file:
+        for line in config:
+            output_file.write(line + "\n")
